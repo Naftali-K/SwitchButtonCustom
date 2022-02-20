66447bdd382966161965f8d37fbe38e11e5a7d16
Naftali <naftali@bk.ru>
2022-02-20 13:31:06 +0200

    init templet

---
+++ app/src/main/java/com/nk/switchcustom/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nk.switchcustom;
+
+/**
+ * Video https://youtu.be/oQ8xPMUEikA
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
